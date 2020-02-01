5993447674903ab84d0c1ed6a0f08ba9454ef8de
Orange Lee <33177912+UnnamedOrange@users.noreply.github.com>
2020-02-01 14:04:19 +0800

    编写新的测试内容。

--- TKernel/UnitTest/Test_TMessageThread.cpp
+++ TKernel/UnitTest/Test_TMessageThread.cpp
@@ -58,5 +58,34 @@ namespace UnitTest
 			cv.wait(lock, [&]()->bool { return test; });
 			Assert::IsTrue(test);
 		}
+		TEST_METHOD(application_test)
+		{
+			struct msgbox
+			{
+				std::wstring text;
+				std::wstring caption;
+			};
+			TMessageThread mt([&](UINT message, std::any param)
+				{
+					switch (message)
+					{
+					case 0:
+					{
+						const auto& [text, caption] = std::any_cast<msgbox>(param);
+						// MessageBoxW(nullptr, text.c_str(), caption.c_str(), NULL);
+						break;
+					}
+					case 1:
+					{
+						MessageBeep(std::any_cast<long>(param));
+						break;
+					}
+					default:
+						break;
+					}
+				});
+			mt.post_message(0, msgbox{ L"233", L"666" });
+			mt.post_message(1, MB_ICONINFORMATION);
+		}
 	};
 }
